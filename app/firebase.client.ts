6f90e886ea621c933aa33b3d31ad354ef714451c
Olivier Lebra <olivier@roakit.com>
2024-02-05 14:47:07 +0100

    Prepare for using secrets

--- app/firebase.client.ts
+++ app/firebase.client.ts
@@ -3,19 +3,19 @@ import { getAuth, browserLocalPersistence, setPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 
 const app = initializeApp({
-    apiKey: 'AIzaSyBTXYXdUDVIO62ZNM4TYxzDBBjq-9tXiUg',
-    authDomain: 'eternal-impulse-412418.firebaseapp.com',
-    projectId: 'eternal-impulse-412418',
-    storageBucket: 'eternal-impulse-412418.appspot.com',
-    messagingSenderId: '957020408701',
-    appId: '1:957020408701:web:7fefaf10eddcb0bf509a14'
+  apiKey: 'AIzaSyBTXYXdUDVIO62ZNM4TYxzDBBjq-9tXiUg',
+  authDomain: 'eternal-impulse-412418.firebaseapp.com',
+  projectId: 'eternal-impulse-412418',
+  storageBucket: 'eternal-impulse-412418.appspot.com',
+  messagingSenderId: '957020408701',
+  appId: '1:957020408701:web:7fefaf10eddcb0bf509a14',
 });
 
 const auth = getAuth(app);
 
 // Note: we copy the JWT to our cookie for server-side Remix, so this would work with no persistence here but...
 // ...for the rest such as Firestore client-side access, we use this persistence
- setPersistence(auth, browserLocalPersistence);
+setPersistence(auth, browserLocalPersistence);
 
 const firestore = getFirestore(app);
 
--- app/firebase.server.ts
+++ app/firebase.server.ts
@@ -1,4 +1,4 @@
-import { App, initializeApp, getApps, cert, getApp } from 'firebase-admin/app';
+import { App, initializeApp, getApps, getApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
@@ -6,6 +6,7 @@ let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
+    // credential: cert(JSON.parse(`${process.env.FIREBASE_SERVICE_ACCOUNT}`)),
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     credential: cert(require('../firebase-service-account.json')),
   });
