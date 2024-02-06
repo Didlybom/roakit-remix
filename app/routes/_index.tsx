e86fa26b62e4b185339f5d3f4003d6c3aab54e96
Olivier Lebra <olivier@roakit.com>
2024-02-06 14:16:16 +0100

    Add realtime and datagrid presentation of GitHub data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { QuerySnapshot, collection, getDocs } from 'firebase/firestore';
+import { collection, QuerySnapshot, onSnapshot, query } from 'firebase/firestore';
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Button, LinearProgress, Stack, Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { sessionCookie } from '~/cookies.server';
-import { auth as serverAuth, firestore as firestoreServer } from '~/firebase.server';
+import { auth as serverAuth } from '~/firebase.server';
 import { firestore as firestoreClient } from '~/firebase.client';
 
 // https://remix.run/docs/en/main/route/meta
@@ -16,20 +17,44 @@ export const meta: MetaFunction = () => [
   { name: 'description', content: 'ROAKIT Prototype' },
 ];
 
-const stringifyDocuments = (snapshot: QuerySnapshot | FirebaseFirestore.QuerySnapshot) => {
-  let response = '';
-  snapshot.forEach((doc) => {
-    response += `${doc.id} => ${JSON.stringify(doc.data())}`;
-  });
-  return response;
-};
-
 type ServerData = {
   isLoggedIn: boolean;
-  firestoreData?: string;
 };
 
-// verify jwt and fetch Firestore
+const githubColumns: GridColDef[] = [
+  { field: 'id', headerName: 'id', width: 250 },
+  {
+    field: 'timestamp',
+    headerName: 'Date',
+    type: 'dateTime',
+    valueGetter: (params) => new Date(params.value),
+    width: 200,
+  },
+  { field: 'repositoryName', headerName: 'Repositoy' },
+  { field: 'author', headerName: 'Author' },
+  { field: 'commit', headerName: 'Activity', width: '100%' },
+];
+
+const githubRows = (snapshot: QuerySnapshot) => {
+  const data = [];
+  snapshot.forEach((doc) => {
+    if (!doc.id.startsWith('github:') || !doc.id.includes(':push:')) {
+      return [];
+    }
+    const docData = doc.data();
+    const props = docData.properties;
+    data.push({
+      id: doc.id,
+      timestamp: docData.eventTimestamp,
+      repositoryName: props.repository?.name,
+      author: props.pusher?.name || props.pusher?.email,
+      commit: props.commits[0]?.message,
+    });
+  });
+  return data;
+};
+
+// verify jwt
 export const loader = async ({ request }: LoaderFunctionArgs): Promise<ServerData> => {
   const jwt = await sessionCookie.parse(request.headers.get('Cookie'));
   if (!jwt) {
@@ -37,8 +62,7 @@ export const loader = async ({ request }: LoaderFunctionArgs): Promise<ServerDat
   }
   try {
     await serverAuth.verifySessionCookie(jwt);
-    const firestoreData = stringifyDocuments(await firestoreServer.collection('olebra_dev1').get());
-    return { isLoggedIn: true, firestoreData };
+    return { isLoggedIn: true };
   } catch (e) {
     return { isLoggedIn: false };
   }
@@ -47,16 +71,17 @@ export const loader = async ({ request }: LoaderFunctionArgs): Promise<ServerDat
 // https://remix.run/docs/en/main/file-conventions/routes#basic-routes
 export default function Index() {
   const serverData = useLoaderData<typeof loader>();
-  const [fireStoreData, setFirestoreData] = React.useState('');
+  const [githubData, setGithubData] = React.useState<any[]>([]);
 
+  // Firestore listener
   React.useEffect(() => {
-    const fetchFirestore = async () => {
-      const response = stringifyDocuments(
-        await getDocs(collection(firestoreClient, 'olebra_dev1')),
-      );
-      setFirestoreData(response);
+    const q = query(collection(firestoreClient, 'events'));
+    const unsuscribe = onSnapshot(q, (querySnapshot) => {
+      setGithubData(githubRows(querySnapshot));
+    });
+    return () => {
+      unsuscribe();
     };
-    fetchFirestore();
   }, []);
 
   return (
@@ -87,16 +112,24 @@ export default function Index() {
         </Stack>
       </Box>
       {serverData.isLoggedIn && (
-        <Stack sx={{ mt: 10, mb: 10 }}>
-          <Typography variant="body1" component="div">
-            Firestore data fetched from client: {fireStoreData}
+        <Stack sx={{ mt: 5 }}>
+          <Typography variant="h6" color="GrayText">
+            GitHub Activity
           </Typography>
-          <Typography variant="body1" component="div">
-            Firestore data fetched from server: {serverData.firestoreData}
-          </Typography>
+          {githubData.length ?
+            <DataGrid
+              columns={githubColumns}
+              rows={githubData}
+              initialState={{
+                sorting: {
+                  sortModel: [{ field: 'timestamp', sort: 'desc' }],
+                },
+              }}
+            ></DataGrid>
+          : <LinearProgress sx={{ mt: 5 }} />}
         </Stack>
       )}
-      <Typography variant="h6" component="div" sx={{ mt: 10, mb: 10 }}>
+      <Typography variant="h6" sx={{ mt: 5, mb: 5 }}>
         Under construction...
       </Typography>
     </React.Fragment>
