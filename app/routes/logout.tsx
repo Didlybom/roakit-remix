a2deffdc4da924b325a4438f5365f16efd9ffae9
Olivier Lebra <olivier@roakit.com>
2024-02-18 17:29:05 +0100

    Beautify logout

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,3 +1,5 @@
+import { Box, Typography } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import { redirect } from '@remix-run/node';
 import { useSubmit } from '@remix-run/react';
 import { useEffect } from 'react';
@@ -24,4 +26,22 @@ export default function Logout() {
     void signOutFromGoogle();
     submit({}, { method: 'post' }); // handle to server to redirect and clear cookie
   }, [submit]);
+
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', m: 10 }}>
+      <Box sx={{ position: 'relative' }}>
+        <Typography>Logging out...</Typography>
+        <CircularProgress
+          size={60}
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginTop: '-30px',
+            marginLeft: '-30px',
+          }}
+        />
+      </Box>
+    </Box>
+  );
 }
