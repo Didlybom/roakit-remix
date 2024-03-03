a611de8d129e00ff90228fac276b7800856ce74f
Olivier Lebra <olivier@roakit.com>
2024-03-03 18:55:17 +0100

    Position footer to the bottom

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Stack } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { ReactNode } from 'react';
@@ -6,10 +7,11 @@ import Copyright from './Copyright';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <Container maxWidth="xl" disableGutters>
-      <Box id="222" sx={{ mb: 1 }}>
-        {children}
+      <Stack direction="column" sx={{ minHeight: '100vh' }}>
+        <Box>{children}</Box>
+        <Box sx={{ flexGrow: 1 }} />
         <Copyright />
-      </Box>
+      </Stack>
     </Container>
   );
 }
