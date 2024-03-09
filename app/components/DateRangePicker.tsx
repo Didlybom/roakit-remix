6725856bb494631524a6b319e081cf6f358c49e0
Olivier Lebra <olivier@roakit.com>
2024-03-09 17:00:37 +0100

    Beautify date range picker

--- app/components/DateRangePicker.tsx
+++ app/components/DateRangePicker.tsx
@@ -1,33 +1,72 @@
-import { CircularProgress, FormControl, MenuItem, Select } from '@mui/material';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import TodayIcon from '@mui/icons-material/Today';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import { useState } from 'react';
 import { DateRange, dateRanges } from '~/utils/dateUtils';
+
+const icons: Record<DateRange, JSX.Element> = {
+  [DateRange.TwoWeeks]: <CalendarMonthIcon fontSize="small" />,
+  [DateRange.OneWeek]: <DateRangeIcon fontSize="small" />,
+  [DateRange.OneDay]: <TodayIcon fontSize="small" />,
+};
 
 export default function DateRangePicker({
   dateRange,
-  showProgress,
   onDateRangeSelect,
+  showProgress,
 }: {
   dateRange: DateRange;
+  onDateRangeSelect: (dateRange: DateRange) => void;
   showProgress?: boolean;
-  onDateRangeSelect?: (dateRange: DateRange) => void;
 }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
   return (
-    <FormControl sx={{ mx: 2, minWidth: 80 }} variant="standard">
-      <Select
+    <Box sx={{ mx: 2, minWidth: 80 }}>
+      <Button
+        color="inherit"
+        startIcon={icons[dateRange]}
+        endIcon={<KeyboardArrowDownIcon />}
+        onClick={handleClickListItem}
+        sx={{ textWrap: 'nowrap' }}
+      >
+        {dateRanges[dateRange]}
+      </Button>
+      <Menu
         id="date-range"
-        value={dateRange}
-        onChange={e => onDateRangeSelect?.(e.target.value as DateRange)}
-        disableUnderline={true}
-        sx={{
-          color: 'inherit',
-          '& .MuiSvgIcon-root': { color: 'inherit' },
-        }}
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        sx={{ color: 'inherit' }}
       >
         {Object.keys(dateRanges).map((date, i) => (
-          <MenuItem key={i} value={date} color="red">
-            {dateRanges[date as DateRange]}
+          <MenuItem
+            key={i}
+            value={date}
+            selected={(date as DateRange) === dateRange}
+            onClick={() => {
+              setAnchorEl(null);
+              onDateRangeSelect(date as DateRange);
+            }}
+          >
+            <ListItemIcon>{icons[date as DateRange]}</ListItemIcon>
+            <ListItemText>{dateRanges[date as DateRange]}</ListItemText>
           </MenuItem>
         ))}
-      </Select>
+      </Menu>
       {showProgress && (
         <CircularProgress
           size={30}
@@ -41,6 +80,6 @@ export default function DateRangePicker({
           }}
         />
       )}
-    </FormControl>
+    </Box>
   );
 }
