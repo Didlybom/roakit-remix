049978c16ba4001533da15a62c06d90cd0ad8ccd
Olivier Lebra <olivier@roakit.com>
2024-03-15 11:15:09 +0100

    Add retry to Firestore fetchers

--- app/firestore.server/fetchers.server.ts
+++ app/firestore.server/fetchers.server.ts
@@ -1,3 +1,5 @@
+import retry from 'async-retry';
+import pino from 'pino';
 import { firestore } from '../firebase.server';
 import {
   ActorData,
@@ -7,75 +9,116 @@ import {
   initiativeSchema,
 } from '../schemas/schemas';
 
+const logger = pino({ name: 'firestore:fetchers' });
+
+const retryProps = {
+  // see https://github.com/tim-kos/node-retry#api
+  retries: 2,
+  factor: 2,
+  minTimeout: 500,
+};
+
 export const fetchInitiatives = async (customerId: number | undefined) => {
-  const coll = firestore.collection(`customers/${customerId}/initiatives`);
-  const docs = await coll.get();
-  const initiatives: InitiativeData[] = [];
-  docs.forEach(initiative => {
-    const data = initiativeSchema.parse(initiative.data());
-    initiatives.push({
-      id: initiative.id,
-      label: data.label,
-      counters:
-        data.counters ?
-          { activities: data.counters.activities }
-        : { activities: { code: 0, codeOrg: 0, task: 0, taskOrg: 0 } },
-      countersLastUpdated: data.countersLastUpdated ?? 0,
-    });
-  });
-  return initiatives.sort((a, b) => a.id.localeCompare(b.id));
+  return await retry(
+    async () => {
+      const coll = firestore.collection(`customers/${customerId}/initiatives`);
+      const docs = await coll.get();
+      const initiatives: InitiativeData[] = [];
+      docs.forEach(initiative => {
+        const data = initiativeSchema.parse(initiative.data());
+        initiatives.push({
+          id: initiative.id,
+          label: data.label,
+          counters:
+            data.counters ?
+              { activities: data.counters.activities }
+            : { activities: { code: 0, codeOrg: 0, task: 0, taskOrg: 0 } },
+          countersLastUpdated: data.countersLastUpdated ?? 0,
+        });
+      });
+      return initiatives.sort((a, b) => a.id.localeCompare(b.id));
+    },
+    {
+      ...retryProps,
+      onRetry: e => logger.warn(`Retrying fetchInitiatives... ${e.message}`),
+    }
+  );
 };
 
 export const fetchInitiativeMap = async (customerId: number | undefined) => {
-  const coll = firestore.collection(`customers/${customerId}/initiatives`);
-  const docs = await coll.get();
-  const initiatives: InitiativeMap = {};
-  docs.forEach(initiative => {
-    const data = initiativeSchema.parse(initiative.data());
-    initiatives[initiative.id] = {
-      label: data.label,
-      counters:
-        data.counters ?
-          { activities: data.counters.activities }
-        : { activities: { code: 0, codeOrg: 0, task: 0, taskOrg: 0 } },
-      countersLastUpdated: data.countersLastUpdated ?? 0,
-    };
-  });
-  return initiatives;
+  return await retry(
+    async () => {
+      const coll = firestore.collection(`customers/${customerId}/initiatives`);
+      const docs = await coll.get();
+      const initiatives: InitiativeMap = {};
+      docs.forEach(initiative => {
+        const data = initiativeSchema.parse(initiative.data());
+        initiatives[initiative.id] = {
+          label: data.label,
+          counters:
+            data.counters ?
+              { activities: data.counters.activities }
+            : { activities: { code: 0, codeOrg: 0, task: 0, taskOrg: 0 } },
+          countersLastUpdated: data.countersLastUpdated ?? 0,
+        };
+      });
+      return initiatives;
+    },
+    {
+      ...retryProps,
+      onRetry: e => logger.warn(`Retrying fetchInitiativeMap... ${e.message}`),
+    }
+  );
 };
 
 export const fetchActors = async (customerId: number | undefined) => {
-  const actors: ActorData[] = [];
-
-  await Promise.all([
+  return await retry(
     async () => {
-      const gitHubColl = firestore.collection(`customers/${customerId}/feeds/1/accounts`);
-      const gitHubDocs = await gitHubColl.get();
-      gitHubDocs.forEach(actor => {
-        const data = actorSchema.parse(actor.data());
-        actors.push({ id: actor.id, name: data.accountName });
-      });
-    },
-    async () => {
-      const jiraColl = firestore.collection(`customers/${customerId}/feeds/2/accounts`);
-      const jiraDocs = await jiraColl.get();
-      jiraDocs.forEach(actor => {
-        const data = actorSchema.parse(actor.data());
-        actors.push({ id: actor.id, name: data.accountName });
-      });
-    },
-  ]);
+      const actors: ActorData[] = [];
 
-  return actors;
+      await Promise.all([
+        async () => {
+          const gitHubColl = firestore.collection(`customers/${customerId}/feeds/1/accounts`);
+          const gitHubDocs = await gitHubColl.get();
+          gitHubDocs.forEach(actor => {
+            const data = actorSchema.parse(actor.data());
+            actors.push({ id: actor.id, name: data.accountName });
+          });
+        },
+        async () => {
+          const jiraColl = firestore.collection(`customers/${customerId}/feeds/2/accounts`);
+          const jiraDocs = await jiraColl.get();
+          jiraDocs.forEach(actor => {
+            const data = actorSchema.parse(actor.data());
+            actors.push({ id: actor.id, name: data.accountName });
+          });
+        },
+      ]);
+
+      return actors;
+    },
+    {
+      ...retryProps,
+      onRetry: e => logger.warn(`Retrying fetchActors... ${e.message}`),
+    }
+  );
 };
 
 export const fetchActorMap = async (customerId: number | undefined) => {
-  const coll = firestore.collection(`customers/${customerId}/feeds/2/accounts`);
-  const docs = await coll.get();
-  const actors: Record<ActorData['id'], Omit<ActorData, 'id'>> = {};
-  docs.forEach(actor => {
-    const data = actorSchema.parse(actor.data());
-    actors[actor.id] = { name: data.accountName };
-  });
-  return actors;
+  return await retry(
+    async () => {
+      const coll = firestore.collection(`customers/${customerId}/feeds/2/accounts`);
+      const docs = await coll.get();
+      const actors: Record<ActorData['id'], Omit<ActorData, 'id'>> = {};
+      docs.forEach(actor => {
+        const data = actorSchema.parse(actor.data());
+        actors[actor.id] = { name: data.accountName };
+      });
+      return actors;
+    },
+    {
+      ...retryProps,
+      onRetry: e => logger.warn(`Retrying fetchActorMap... ${e.message}`),
+    }
+  );
 };
