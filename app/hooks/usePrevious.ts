26d7a94d7000dbbb666e91f3a7a4dae3382fa29f
Olivier Lebra <olivier@roakit.com>
2024-05-08 12:52:25 +0200

    Rework usePrevious

--- app/hooks/usePrevious.ts
+++ app/hooks/usePrevious.ts
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
-export const usePrevious = <T>(value: T) => {
-  const ref = useRef<T>();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
+export const usePrevious = <T>(value: T, isEqual?: (a: T, b: T) => boolean) => {
+  const ref = useRef<{ current: T; previous: T | null }>({ current: value, previous: null });
+  const current = ref.current.current;
+  if (!isEqual?.(value, current) ?? value !== current) {
+    ref.current = { current: value, previous: current };
+  }
+  return ref.current.previous;
 };
