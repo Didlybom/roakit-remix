7495a9af55f9eec0f166f37823de64f9f8b2ca3f
Olivier Lebra <olivier@roakit.com>
2024-07-04 20:36:20 +0200

    Tweak Roakit logo
    Tweak Roakit logo

--- app/components/navigation/Header.tsx
+++ app/components/navigation/Header.tsx
@@ -78,7 +78,7 @@ export default function Header({
           </IconButton>
         )}
         <Box title="Roakit" mr={2} display={{ xs: 'none', sm: 'flex' }}>
-          <RoakitIcon width="45px" height="19px" />
+          <RoakitIcon width="51px" height="20px" />
         </Box>
         {view !== View.Login && view !== View.Logout && (
           <>
