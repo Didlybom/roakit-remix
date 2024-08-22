618cd3f9641d9e4e41d673755ba319a4bef666a2
Olivier Lebra <olivier@roakit.com>
2024-08-22 14:01:12 +0200

    Clean up unused code

--- app/firebase.client.ts
+++ app/firebase.client.ts
@@ -1,15 +1,12 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
 
 const app = firebase.initializeApp(window.ROAKIT_ENV.firebase);
 
 const clientAuth = app.auth();
 clientAuth.languageCode = 'en'; // auth.useDeviceLanguage();
 
-// We copy the JWT to our cookie for server-side Remix
+// We copy the JWT to our own cookie for server-side Remix
 void clientAuth.setPersistence(firebase.auth.Auth.Persistence.NONE);
 
-const firestore = firebase.firestore(app);
-
-export { clientAuth, firestore };
+export { clientAuth };
